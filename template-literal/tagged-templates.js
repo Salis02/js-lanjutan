ebdab01f99412ea0aa7f26df81cde1dd3a84893c
Salis02 <nizarsalis@gmail.com>
2025-10-17 15:34:47 +0700

    tagged template implementasi contoh

--- template-literal/tagged-templates.js
+++ template-literal/tagged-templates.js
@@ -1,5 +1,8 @@
 // TAGGED TEMPLATES LITERAL
 
+// 1. Konsep singkat
+// Tagged Template Literal adalah fungsi khusus yang bisa digunakan untuk memproses template literal (``) sebelum hasil akhirnya dikembalikan.
+
 const identitas = {
     nama: 'Salis Nizar Qomaruzaman',
     email: 'nizarsalis@gmail.com'
@@ -14,4 +17,18 @@ const testing = (strings, ...value) => {
 
 const test = testing`Saya ${identitas.nama}, email saya ${identitas.email}`;
 
-document.body.innerHTML = test;
+// document.body.innerHTML = test;
+
+// 2. Implementasi di case yang lain =>  hightlight (sorotan) pada text dinamis
+
+const hightlight = (strings, ...value) => {
+    return strings.reduce(
+        (result, str, i) =>
+            `${result}${str}<span style="background-color: yellow; font-weight:bold;" >${value[i] || ''}</span>` , ''
+    )
+}
+
+const nama = 'Salis Nizar Qomaruzaman';
+const hobi = 'Programming';
+
+document.body.innerHTML = hightlight`Halo ${nama}, hobimu ${hobi}, kan?`;
