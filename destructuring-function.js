8248186743def91ea565766f1af6086c943b4ba7
Salis02 <nizarsalis@gmail.com>
2025-10-20 10:41:46 +0700

    berbagai macam cara kombinasi dalam destructuring arrray dan object

--- destructuring-function.js
+++ destructuring-function.js
@@ -8,8 +8,23 @@ const name = [
 ]
 
 const [namaAwal, namaTengah, namaAkhir] = name;
-
 // console.log(namaAwal, namaTengah, namaAkhir);
+
+// Skipping item
+const [Awal, , Akhir] = name;
+
+// Swap items => tukar posisi
+let a = 1;
+let b = `Selamat pagi, ${namaAwal}`;
+
+[b, a] = [a, b]
+// console.log(a, b);
+
+// REST PARAMETER
+const hewan = ['kucing', 'harimau', 'anaconda', 'gajah', 'kupu-kupu']
+
+const [hewanPertama, ...value] = hewan;
+// console.log(...value);
 
 // 2. OBJEK = const {....}, dan harus sama nama variable, beda harus memakai alias
 
@@ -21,8 +36,7 @@ const pegawai = {
 
 // Destructuring nama variable sama
 const { nama, email, jabatan } = pegawai;
-
-console.log(nama, email, jabatan);
+// console.log(nama, email, jabatan);
 
 // Destructuring nama dengan alias
 const {
@@ -30,5 +44,30 @@ const {
     email: emailPegawai,
     jabatan: jabatanPegawai
 } = pegawai;
+// console.log(namaPegawai, emailPegawai, jabatanPegawai);
 
-console.log(namaPegawai, emailPegawai, jabatanPegawai);
+// Assigntment tanpa declarration object
+({ menikah, umur } = { menikah: 'Lajang', umur: '24' });
+// console.log(status);
+
+// Default value
+const mahasiswa = {
+    prodi: 'informatika',
+    kondisi: 'lulus'
+}
+
+const { prodi, kondisi = 'active' } = mahasiswa;
+// console.log(kondisi);
+
+const data = {
+    id: 147,
+    nama: 'Salis Nizar Qomaruzaman',
+    pekerjaan: 'Programmer'
+}
+
+function getName({ nama, pekerjaan }) {
+    alert(`Data ${nama} ditemukan! Subjek adalah seorang ${pekerjaan}`);
+}
+
+getName(data);
+
